e69c1f1296ae3832fe8d9b56176aa015fba6a54f
tasplo <tasplo@proton.me>
2024-10-10 09:10:58 +0300

    update title for landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
-<div
-	class="w-full h-screen flex items-center justify-center flex-col gap-2"
->
+<svelte:head>
+	<title>Checkers Game</title>
+</svelte:head>
+
+<div class="w-full h-screen flex items-center justify-center flex-col gap-2">
 	<p class="text-center text-xl">Hello there!! 👋</p>
 	<p class="text-center">Welcome to Checkers Game</p>
 	<a href="/game" type="button" class="px-5 py-3 text-sm bg-white text-zinc-950 mt-5">Start Game</a>
